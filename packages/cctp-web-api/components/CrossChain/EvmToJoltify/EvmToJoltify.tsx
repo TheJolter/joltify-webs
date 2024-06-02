4d964c83b0e755da3a4480944d27a34c641e6364
Eason Smith <eason@smi.th>
2024-06-02 16:21:43 +0800

    renew source balance after evm send, no need to wait noble received then renew balance

--- packages/cctp-web-api/components/CrossChain/EvmToJoltify/EvmToJoltify.tsx
+++ packages/cctp-web-api/components/CrossChain/EvmToJoltify/EvmToJoltify.tsx
@@ -1,11 +1,31 @@
 import { Button } from "@nextui-org/react"
+import { useState } from "react"
+import { Spinner, Link } from "@nextui-org/react";
 
 export default function EvmToJolyify() {
+  const [nobleReceived, setNobleReceived] = useState(false)
+  const [sendingToNoble, setSendingToNoble] = useState(false)
+  const [sendingToJoltify, setSendingToJoltify] = useState(false)
+
   return (
 <div className="grid grid-cols-3">
-  <Button color="success">1. Send to Noble</Button>
-  <div className="flex items-center justify-center text-gray-500">2. Received on Noble</div>
-  <Button disabled>3. IBC to Joltify</Button>
+  <Button
+    disabled={sendingToNoble||nobleReceived}
+    color={nobleReceived?'default':'success'}
+  >
+    1. Send to Noble
+    {sendingToNoble&&<Spinner size="sm" color="default"/>}
+  </Button>
+  <div className={`flex items-center justify-center ${!nobleReceived&&'text-gray-500'}`}>
+    2. Received on Noble
+  </div>
+  <Button 
+    disabled={!nobleReceived||sendingToJoltify}
+    color={nobleReceived?'success':'default'}
+  >
+    3. IBC to Joltify
+    {sendingToJoltify&&<Spinner size="sm" color="default"/>}
+  </Button>
 </div>
   )
 }
