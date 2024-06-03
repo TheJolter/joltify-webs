0626c8fc20aba081d305abe3e5ef368655c5fb82
Eason Smith <eason@smi.th>
2024-06-03 16:46:29 +0800

    is cosmos address return true if checked and without prefix

--- packages/cctp-web-api/utils/isCosmosAddress.ts
+++ packages/cctp-web-api/utils/isCosmosAddress.ts
@@ -2,11 +2,12 @@ import { normalizeBech32 } from '@cosmjs/encoding'
 
 export default function isCosmosAddress({address, prefix}: {
   address: string,
-  prefix: string
+  prefix?: string
 }): boolean {
   try {
     normalizeBech32(address)
-    return address.startsWith(prefix)
+    if (prefix) return address.startsWith(prefix)
+    return true
   } catch(e) {
     return false
   }
