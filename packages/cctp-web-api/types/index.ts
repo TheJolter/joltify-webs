f79d656ffb9062d9182fcf5b4d7309a4ae005dbe
Eason Smith <eason@smi.th>
2024-06-03 22:42:28 +0800

    common type of cctp attastation

--- packages/cctp-web-api/types/index.ts
+++ packages/cctp-web-api/types/index.ts
@@ -3,3 +3,9 @@ import { SVGProps } from "react";
 export type IconSvgProps = SVGProps<SVGSVGElement> & {
   size?: number;
 };
+
+export type Attestation = { // eg: https://iris-api.circle.com/v1/messages/1/0x30a284a1856b5ca31ee402691970823d2177d23f3bc996c6543114965f660fcd
+  attestation: string,
+  message: string,
+  eventNonce: string
+}
