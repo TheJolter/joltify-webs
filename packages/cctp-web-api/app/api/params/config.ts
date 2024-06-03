9fb32d4800ede209e2acee9c2df97e369013a974
Eason Smith <eason@smi.th>
2024-06-04 00:53:55 +0800

    recover to nomal fee

--- packages/cctp-web-api/app/api/params/config.ts
+++ packages/cctp-web-api/app/api/params/config.ts
@@ -9,13 +9,13 @@ export const params = {
     },
     {
       chainName: 'Avalanche',
-      fee: '1',
+      fee: '1000000',
       time: "20 seconds",
       domain: 1, 
     },
     {
       chainName: 'Arbitrum',
-      fee: '1',
+      fee: '1000000',
       time: "13 minutes",
       domain: 3 
     }
