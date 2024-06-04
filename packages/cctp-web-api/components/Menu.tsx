415f7113629f80c3b1a6249719f56a278af73db8
Eason Smith <eason@smi.th>
2024-06-04 15:18:22 +0800

    show logo and menu like pathr

---
+++ packages/cctp-web-api/components/Menu.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import { useTheme } from "next-themes"
+import { CSSProperties, useEffect, useState } from "react"
+import MainButton from "./MainButton"
+import { observer } from "mobx-react-lite"
+
+export default observer(function Menu() {
+  const { theme } = useTheme()
+  const [menuBgStyle, setMenuBgStyle] = useState<CSSProperties>()
+  const noneSelectedStyle = {background: 'rgba(0,0,0,0)', color: '#9FA8AB'}
+
+  useEffect(()=>{
+    if (theme==='dark') {
+      setMenuBgStyle({
+        background: 'linear-gradient(90deg, #32CA62 0%, #EAF83F 100%)', 
+        opacity: 0.06
+      })
+      return
+    }
+    setMenuBgStyle({
+      background: 'linear-gradient(90deg, #0A5020 0%, #353808 100%)', 
+      opacity: 0.06
+    })
+  }, [theme])
+  return (<>
+<div id="menu-buttons" className="relative min-w-[300px]">
+  <div className="relative z-10 px-[6px] flex items-center h-[58px]">
+    <MainButton className="h-[46px] font-semibold text-lg" fullWidth
+      onClick={()=>window.open('https://app.pathr.io')}
+    >Swap</MainButton>
+    <MainButton className="h-[46px] font-semibold text-lg" fullWidth
+      style={noneSelectedStyle}
+    >USDC</MainButton>
+  </div>
+  <div className="rounded-full absolute w-full h-[58px] top-0" 
+    style={menuBgStyle}
+  ></div>
+</div>
+</>)
+})
--- packages/cctp-web-api/components/navbar.tsx
+++ packages/cctp-web-api/components/navbar.tsx
@@ -4,29 +4,23 @@ import {
   NavbarBrand,
   NavbarItem,
 } from "@nextui-org/navbar";
-import NextLink from "next/link";
 import { ThemeSwitch } from "@/components/theme-switch";
-import { Button } from "@nextui-org/react";
 import EvmWalletButton from "./EvmWalletButton/EvmWalletButton";
 import CosmosWalletButton from "./CosmosWalletButton/CosmosWalletButton";
-import Link from "next/link";
+import Logo from "./Logo/Logo";
+import Menu from "./Menu";
 
 export const Navbar = () => {
   return (
     <NextUINavbar maxWidth="xl" position="sticky">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
         <NavbarBrand as="li" className="gap-3 max-w-fit">
-          <NextLink className="flex justify-start items-center gap-1" href="/">
-            <img src="/images/logo-pathr.png" style={{height: '32px'}} alt="" />
-            <p className="font-bold text-inherit pl-2">USDC Bridge</p>
-          </NextLink>
+          <Logo />
         </NavbarBrand>
       </NavbarContent>
 
-      <NavbarContent justify="end">
-        <NavbarItem>
-          <Link href='https://app.pathr.io' target="_blank" className="underline font-bold">Pathr</Link>
-        </NavbarItem>
+      <NavbarContent justify="center">
+        <Menu />
       </NavbarContent>
 
       <NavbarContent
