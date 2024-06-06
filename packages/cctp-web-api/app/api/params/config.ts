2a6c754e48d30e18bf8eeee0fc6defdc92c59308
Eason Smith <eason@smi.th>
2024-06-06 16:32:40 +0800

    in dev evn, use 1uusdc as route fee

--- packages/cctp-web-api/app/api/params/config.ts
+++ packages/cctp-web-api/app/api/params/config.ts
@@ -3,19 +3,19 @@ export const params = {
   targetChains: [
     {
       chainName: 'Ethereum',
-      fee: '20000000', // gas fee eg: https://etherscan.io/tx/0x499731e953c645b7fd77092943de16202a781ca38dd3031b854f586b8f789625
+      fee: location.hostname==='localhost'?'1':'20000000', // gas fee eg: https://etherscan.io/tx/0x499731e953c645b7fd77092943de16202a781ca38dd3031b854f586b8f789625
       time: "13 minutes",
       domain: 0 // https://developers.circle.com/stablecoins/docs/supported-domains
     },
     {
       chainName: 'Avalanche',
-      fee: '1000000',
+      fee: location.hostname==='localhost'?'1':'1000000',
       time: "20 seconds",
       domain: 1, 
     },
     {
       chainName: 'Arbitrum',
-      fee: '1000000',
+      fee: location.hostname==='localhost'?'1':'1000000',
       time: "13 minutes",
       domain: 3 
     }
