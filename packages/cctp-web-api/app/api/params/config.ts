765435b89857ad1ce907e859fc88c3f79ff26b7d
Eason Smith <eason@smi.th>
2024-06-11 15:54:48 +0800

    use evn param to set fees on dev mode for better test

--- packages/cctp-web-api/app/api/params/config.ts
+++ packages/cctp-web-api/app/api/params/config.ts
@@ -3,19 +3,19 @@ export const params = {
   targetChains: [
     {
       chainName: 'Ethereum',
-      fee: '20000000', // gas fee eg: https://etherscan.io/tx/0x499731e953c645b7fd77092943de16202a781ca38dd3031b854f586b8f789625
+      fee: process.env.NEXT_PUBLIC_ENV==='DEV'?'1':'20000000', // gas fee eg: https://etherscan.io/tx/0x499731e953c645b7fd77092943de16202a781ca38dd3031b854f586b8f789625
       time: "13 minutes",
       domain: 0 // https://developers.circle.com/stablecoins/docs/supported-domains
     },
     {
       chainName: 'Avalanche',
-      fee: '1000000', // 1000000 // can not access location.hostname on server
+      fee: process.env.NEXT_PUBLIC_ENV==='DEV'?'1':'1000000', // can not access location.hostname on server
       time: "20 seconds",
       domain: 1, 
     },
     {
       chainName: 'Arbitrum',
-      fee: '1000000', // 1000000
+      fee: process.env.NEXT_PUBLIC_ENV==='DEV'?'1':'1000000',
       time: "13 minutes",
       domain: 3 
     }
